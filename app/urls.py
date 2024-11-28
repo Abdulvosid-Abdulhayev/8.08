116eba57f0a246bab65e5912fa1265920a0362c8
Abudlvosid Abdulhayev <abdulvosid780@gmail.com>
2024-11-28 13:04:54 +0500

    change for month 8 lesson 9 homowerk

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import FoodTypeView, FoodView, CommentView
+from .views import FoodTypeView,FoodTypeDetailView, FoodView,FoodDetailView, CommentView, CommentDetailView
 
 urlpatterns = [
     path('food-types/', FoodTypeView.as_view(), name='foodtype-list'),
-    path('food-types/<int:pk>/', FoodTypeView.as_view(), name='foodtype-detail'),
+    path('food-types/<int:pk>/', FoodTypeDetailView.as_view(), name='foodtype-detail'),
     path('foods/', FoodView.as_view(), name='food-list'),
-    path('foods/<int:pk>/', FoodView.as_view(), name='food-detail'),
+    path('foods/<int:pk>/', FoodDetailView.as_view(), name='food-detail'),
     path('comments/', CommentView.as_view(), name='comment-list'),
-    path('comments/<int:pk>/', CommentView.as_view(), name='comment-detail'),
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,88 +1,46 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import (
-    ListModelMixin,
-    CreateModelMixin,
-    RetrieveModelMixin,
-    UpdateModelMixin,
-    DestroyModelMixin
-)
+from rest_framework import generics
+from rest_framework import filters
+
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import FoodType, Food, Comment
 from .serializers import FoodTypeSerializer, FoodSerializer, CommentSerializer
 
-# FoodType uchun View
-class FoodTypeView(GenericAPIView, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
-    """
-    FoodType modeliga oid view:
-    - GET (barcha yoki bitta turni ko'rish)
-    - POST (yangi tur qo'shish)
-    - PUT/PATCH (tur ma'lumotlarini yangilash)
-    - DELETE (tur o'chirish)
-    """
+class FoodTypeView(generics.ListCreateAPIView):
+    queryset = FoodType.objects.all()
+    serializer_class = FoodTypeSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    searching_fields = "__all__"
+    ordering_fields = "__all__"
+
+class FoodTypeDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = FoodType.objects.all()
     serializer_class = FoodTypeSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get(self, request, *args, **kwargs):
-        if 'pk' in kwargs:
-            return self.retrieve(request, *args, **kwargs)
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-# Food uchun View
-class FoodView(GenericAPIView, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
-    """
-    Food modeliga oid view:
-    - GET (barcha taomlarni yoki bitta taomni ko'rish)
-    - POST (yangi taom qo'shish)
-    - PUT/PATCH (taom ma'lumotlarini yangilash)
-    - DELETE (taom o'chirish)
-    """
+class FoodView(generics.ListCreateAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    searching_fields = "__all__"
+    ordering_fields = "__all__"
 
-    def get(self, request, *args, **kwargs):
-        if 'pk' in kwargs:
-            return self.retrieve(request, *args, **kwargs)
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-# Comment uchun View
-class CommentView(GenericAPIView, ListModelMixin, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin):
-    """
-    Comment modeliga oid view:
-    - GET (barcha izohlarni yoki bitta izohni ko'rish)
-    - POST (izoh qo'shish)
-    - DELETE (izoh o'chirish)
-    """
+class FoodDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+class CommentView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    searching_fields = ["text", "author__username"]
+    ordering_fields = ["created"]
 
-    def get(self, request, *args, **kwargs):
-        if 'pk' in kwargs:
-            return self.retrieve(request, *args, **kwargs)
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
